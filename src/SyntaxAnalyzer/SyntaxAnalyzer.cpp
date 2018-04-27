da09e54a2f04ac61505a4f0881b73670a407fd10
Amr Hendy <amr.m.hendy@gmail.com>
2018-04-27 04:02:24 +0200

    Fixing Derivation with previous terminals and substitue with lexical values

--- src/SyntaxAnalyzer/SyntaxAnalyzer.cpp
+++ src/SyntaxAnalyzer/SyntaxAnalyzer.cpp
@@ -32,20 +32,37 @@ SyntaxAnalyzer::analyze_prog(vector<Token> user_prog, DerivationTable derivation
 	stack.push(new Node(start_nt_node, NodeType::NonTerminal));
 	output_file.push_back(stack_to_string(stack));
 
+	string prev_terminals = "";
+
 	int token_index = 0;
 	while(token_index < user_prog.size() && !stack.empty()){
 		Node* curr = stack.top();
 		string current_symbol = "'" + user_prog[token_index].getType() + "'";
+		if(current_symbol == "'$'"){
+			current_symbol = "$";
+		}
 
 		if(curr->get_type() == NodeType::Terminal){
+			if(curr->is_epsilon()){
+				stack.pop();
+				continue;
+			}
+
 			if(curr->get_name() == current_symbol){
+				/* current token matching first non-terminal */
+				stack.pop();
+				token_index++;
+
 				/* check if we finished the input. */
 				if(curr->get_name() == "$"){
 					break;
 				}
-				/* current token matching first non-terminal */
-				stack.pop();
-				token_index++;
+				//prev_terminals += user_prog[token_index - 1].getType() + "\t";
+				prev_terminals += user_prog[token_index - 1].getLexeme() + "\t";
+
+				if(user_prog[token_index - 1].getLexeme() != user_prog[token_index - 1].getType()){
+					output_file.push_back(prev_terminals + stack_to_string(stack));
+				}
 			}
 			else{
 				/* unidentified error type, hence skip current token. */
@@ -76,7 +93,7 @@ SyntaxAnalyzer::analyze_prog(vector<Token> user_prog, DerivationTable derivation
 						stack.push(nodes[index]);
 					}
 				}
-				output_file.push_back(stack_to_string(stack));
+				output_file.push_back(prev_terminals + stack_to_string(stack));
 			}
 		}
 	}
@@ -84,6 +101,7 @@ SyntaxAnalyzer::analyze_prog(vector<Token> user_prog, DerivationTable derivation
 	string derivation_file = "derivation.txt";
 	FileWriter::openNewFile(derivation_file);
 	for(string line : output_file){
+		line = "==>\t" + line;
 		FileWriter::append(derivation_file, line);
 	}
 }
@@ -96,7 +114,13 @@ string SyntaxAnalyzer::stack_to_string(stack<Node*> tmp){
 		Node* node = tmp.top();
 		tmp.pop();
 		if(!node->is_dollar() && !node->is_epsilon()){
-			result.append(node->get_name());
+			/* remove the single quotes in case of terminal node. */
+			if(node->get_type() == NodeType::Terminal){
+				result += node->get_name().substr(1, node->get_name().size() - 2) + "\t";
+			}
+			else{
+				result += node->get_name() + "\t";
+			}
 		}
 	}
 	return result;
